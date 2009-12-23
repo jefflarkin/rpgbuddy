18f4dfc10ce242e750d363cd265133b4195ea436
Jeff Larkin <jeff.larkin@gmail.com>
2009-12-23 18:54:32 -0500

    Initial commit.

---
+++ wave_robot.py
@@ -0,0 +1,27 @@
+from waveapi import events
+from waveapi import model
+from waveapi import robot
+from waverpg import *
+
+def OnRobotAdded(properties, context):
+  """Invoked when the robot has been added."""
+  root_wavelet = context.GetRootWavelet()
+  root_wavelet.CreateBlip().GetDocument().SetText("For more information on using RPG Buddy, see http://rpgbuddy.appspot.com/.")
+
+def OnBlipSubmitted(properties, context):
+  blip = context.GetBlipById(properties['blipId'])
+  waveId = blip.GetWaveId()
+  waveletId = blip.GetWaveletId()
+  text = blip.GetDocument().GetText()
+  newtext = process_text(text)
+  blip.GetDocument().SetText(" ")
+  context.builder.DocumentAppendMarkup(waveId, waveletId, blip.GetId(), newtext)
+  
+if __name__ == '__main__':
+  myRobot = robot.Robot('RPG Buddy', 
+      image_url='http://rpgbuddy.appspot.com/dice.gif',
+      version='2',
+      profile_url='http://rpgbuddy.appspot.com/')
+  myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
+  myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmitted)
+  myRobot.Run()
---
+++ waverpg.py
@@ -0,0 +1,45 @@
+import re
+from random import randint
+def process_text(text):
+  r = re.compile("(\[\[.*?\]\])")
+  # Find all rolls in the given text
+  for match in r.findall(text):
+    # Strip out the brackets so we won't match this again later
+    save = str(match)
+    match = str(match).replace("[","").replace("]","")
+    # Get the new roll string
+    result = roll(match.strip())
+    # Substitute roll code for result
+    text = re.sub(re.escape(save),result,text,1)
+  return text
+
+def roll(text):
+  r = re.compile("(\d+)[dD](\d+)[oO]?(\d*)\+?(\d*)")
+  # match all roll codes in the string
+  while 1:
+    match = r.search(text)
+    if match == None: break
+    sum = 0
+    rolls = []
+    output = "["
+    for i in range(int(match.group(1))):
+      last = randint(1,int(match.group(2)))
+      rolls.append(last)
+      sum += last
+      if i == (int(match.group(1))-1):
+        output = "%s <strong style=\"color: red\">%d</strong>"%(output, last)
+      else:
+        output = "%s %d"%(output, last)
+    while (match.group(3) != '') and (last == int(match.group(3))):
+      last = randint(1,int(match.group(2)))
+      rolls.append(last)
+      sum += last
+      output = "%s %d"%(output, last)
+    if match.group(4) != '':
+      sum += int(match.group(4))
+      output = "%s + %s"%(output, match.group(4))
+    output = "%s ] = <strong>%s</strong>"%(output,sum)
+    text = re.sub(re.escape(match.group(0)),output,text,1)
+
+#  return r.sub(output,text)
+  return text
